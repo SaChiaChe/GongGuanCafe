51fedba3d80eadc084bbc1146df22fda5eff644e
SaKa <karta1119371@gmail.com>
2018-06-19 11:41:25 +0800

    UPDATE Random Recommend

---
+++ JS_Script/RandomRecommend.js
@@ -0,0 +1,22 @@
+function RndInt(max) {
+    return Math.floor(Math.random() * max) % max;
+}
+
+db.collection("ForRandomRecommend").onSnapshot(function (querySnapshot) {
+    var TitleList = [];
+    querySnapshot.forEach(function (doc) {
+        TitleList.push(doc.data().Name);
+    });
+    var RndId = RndInt(TitleList.length);
+    var RndTitle = TitleList[RndId];
+    var RndHref = "/pages/" + RndTitle + "/" + RndTitle + ".html";
+    console.log(RndHref);
+    // Retry when Random Recommend to the same page
+    while (RndHref == window.location.href.toString().split(window.location.host)[1]) {
+        RndId = RndInt(TitleList.length);
+        RndTitle = TitleList[RndId];
+        RndHref = "/pages/" + RndTitle + "/" + RndTitle + ".html";
+        console.log(RndHref);
+    }
+    $("#RndRec").attr("href", RndHref);
+});
